2b45a60e0fde8dafdde502e37ebfa11f0f39fe30
Alejandro Bustamante <adbdc.u@gmail.com>
2025-05-08 11:13:44 -0400

    test

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -8,6 +8,6 @@ describe("Sumar", () => {
     expect(sumar(3, 2)).toEqual(5);
   });
   it("deberia sumar dos numeros", () => {
-    expect(sumar(3, 2)).toEqual(5);
+    expect(sumar(3, 2)).toEqual(2);
   });
 });
